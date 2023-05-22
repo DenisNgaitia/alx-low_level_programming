69fc2deaa8ed7e75f1f3c26b3c1c75f5ddbc15f3
DenisNgaitia <dennisngatiantlax@gmail.com>
2023-05-22 09:58:21 -0700

    fixing more errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- *main - A C programme that prints a line to the standard eror
+ *main - A programme that prints a line to the standard eror
  *
  *Return: 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
