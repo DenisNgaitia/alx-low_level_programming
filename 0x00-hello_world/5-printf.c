b0dd85311da69942b26479d756cf190cf3fad51d
DenisNgaitia <dennisngatiantlax@gmail.com>
2023-05-22 07:48:34 -0700

    fixing the error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main -A programme that prints a string
  * Return: 0 (success)
  */
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
