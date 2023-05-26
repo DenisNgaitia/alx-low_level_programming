2cc2fca66bd9c4886c8205b35ca53a5d7be08942
DenisNgaitia <dennisngatiantlax@gmail.com>
2023-05-26 10:12:17 -0700

    fixing some idention errors

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,7 @@
 /**
  * print_diagonal - draws a diagonal line on the terminal
  * @n: number of times the character \ should be printed
- **/
+ */
 
 void print_diagonal(int n)
 {
@@ -11,19 +11,19 @@ void print_diagonal(int n)
 	{
 		_putchar('\n');
 	} else
-		{
-			int i, j;
+	{
+		int i, j;
 
-			for (i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
 			{
-				for (j = 0; j < n; j++)
-						{
-																												if (j == i)
-																													_putchar('\\');
-																												else if (j < i)
-																													_putchar(' ');
-																											}
-				_putchar('\n');
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
 			}
+			_putchar('\n');
 		}
+	}
 }
