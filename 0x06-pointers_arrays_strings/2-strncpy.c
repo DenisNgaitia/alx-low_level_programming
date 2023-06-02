b57946d60c92ec8237f419cf7934e73a109f9712
DenisNgaitia <dennisngatiantlax@gmail.com>
2023-06-02 09:33:34 -0700

    fixing some errors

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/*
+
+/**
  * _strncpy - copy a string
  * @dest: input value
  * @src: input value
@@ -7,10 +8,11 @@
  *
  * Return: dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
-	
+
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,14 +1,16 @@
 #include "main.h"
-/*
+
+/**
  * string_toupper - change all lowercase to uppercase
  * @n: pointer
  *
  * Return: n
  */
+
 char *string_toupper(char *n)
 {
 	int i;
-	
+
 	i = 0;
 	while (n[i] != '\0')
 	{
