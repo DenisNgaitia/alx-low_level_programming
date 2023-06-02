1169c2cdb4d0dc490f97dbbe0aa30725770cb432
DenisNgaitia <dennisngatiantlax@gmail.com>
2023-06-02 09:19:04 -0700

    git push

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/*
+/**
  * _strncat - concatenate two strings
  * using at most n bytes from src
  * @dest: input value
@@ -12,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
-	
+
 	i = 0;
 	while (dest[i] != '\0')
 	{
