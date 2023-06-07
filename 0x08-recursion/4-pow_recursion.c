ef43b4723587807e7224f597614f4a02c28cdf99
DenisNgaitia <dennisngatiantlax@gmail.com>
2023-06-07 06:32:27 -0700

    changed to betty styke

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _pow_recursion - programme that returns the value of x raised to the power of y
+ * _pow_recursion - programme
+ * returns the value of x raised to the power of y
  * @x: This one is the value to raise
  * @y: This is the power
  *
